540b7f0be5303c0a4f3902d52ff9cf4396890f4e
sjlutterbie <simon@ltrbe.com>
2018-07-14 19:00:48 +0000

    Completed cool router test

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var coolRouter = require('./routes/cool');
 
 var app = express();
 
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/users/cool', coolRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
